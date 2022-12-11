26eee7c60a1dee8898645968f89437def3057077
hiago <hiaguinhospencer@gmail.com>
2022-12-10 23:33:09 -0300

    Fizemos as configurações e criamos alguns post

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
-
+LISTA_CATEGORIA = (
+    ('esportes', 'Esportes'),
+    ('noticias', 'Notícias'),
+    ('cinema', 'Cinema'), 
+    ('tecnologia', 'Tecnologia'),
+    ('finanças', 'Finanças'),
+    
+)
 class Blog(models.Model):
     titulo = models.CharField(max_length=1000)
-    mini_descricao = models.TextField(max_length=2000, default=False)
-    descricao = models.TextField(max_length=1000000)
+    mini_descricao = models.CharField(max_length=2000)
+    descricao = RichTextField(max_length=1000000)
     image = models.ImageField(upload_to='thumb_img')
     data = models.DateTimeField(default=timezone.now)
     autor = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=30, choices=LISTA_CATEGORIA)
 
     def __str__(self):
         return self.titulo
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,12 +3,10 @@ from django.views.generic import ListView, DetailView
 from .models import Blog
 
 
-def blog(request):
-    return render(request, 'index.html')
-
 class BlogListView(ListView):
     template_name = 'index.html'
     model = Blog
+    
 
 
 class BlogDetailView(DetailView):
